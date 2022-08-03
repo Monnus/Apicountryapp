5b4bfc36da5784827ab2709e1a5cd369c12e85c5
Monnus <maun2velsket@gmail.com>
2022-08-03 16:19:10 +0200

    clean up

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import { Appbar } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
-import  React,{useState}from 'react';
+import  React,{useState,useEffect}from 'react';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { StyleSheet, Text, View ,SafeAreaView,TextInput} from 'react-native';
 
@@ -8,56 +9,67 @@ import { StyleSheet, Text, View ,SafeAreaView,TextInput} from 'react-native';
 
 export default function App() {
 const [countryName,setCoutryName]=useState("");
+const [display,setDisplay]=useState({})
+const [text,setText] =useState("")
 
-  const renderCountry=function(data){
+
+ const getCountryData= function(country){
+  const response= fetch(`https://restcountries.com/v2/name/${country}`)
+  .then((res)=>{
+    return res.json()
+  }).then((data)=>{
+    console.log(data[0]);
+    return data[0]
+  })
+
+  
+  return response;
+ 
+  }
+  
+  getCountryData("portugal")
+
+  function displayCountry(data){
+
     return(
       <View>
-   <Card>
+         <Card>
       <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
       <Card.Content>
-        <Title>{data.name}</Title>
-        <Paragraph>Card content</Paragraph>
-      </Card.Content>
-      <Card.Cover source={{ uri: `${data.flags.png}`}} />
-      <Card.Actions>
-        <Button icon="" mode="outlined">Cancel</Button>
-        <Button icon="" mode="contained">Get nabour Country</Button>
-      </Card.Actions>
-    </Card>
-  
+      <Title>{data.name}</Title>
+      <Paragraph>
+        {data.name} is country that speakes {data.languages[0].nativeName},
+        {data.name} region is {data.regiion}
+      </Paragraph>
+       </Card.Content>
+        <Card.Cover source={{ uri: `${data.flags.png}` }} />
+       <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+      </Card>
       </View>
     )
   }
-  const getCountryData=function(country){
-    const request=fetch(`https://restcountries.com/v2/name/${country}`)
-    .then((res)=>{
-      return res.json();
-    }).then((data)=>{
-      console.log(data[0]);
-    })
-  
-  }
-  getCountryData("portugal");
-  
-const handleSearch=function(data=""){
-  if(data==="")return alert("insert a countries name")
- return renderCountry(data);
-}
-
-
-// console.log(handleSearch(countryName));
 
   return (
     <SafeAreaView style={styles.container}>
+       <Appbar.Header style={{marginTop:40, backgroundColor:'blue'}}>
+      
+      <Appbar.Content title="Home" />
+      
+    </Appbar.Header>
      <View style={styles.searchbar}>
       <TextInput
       placeholder='Search Buy Name'
       onChangeText={setCoutryName}
       style={{color:"white"}}
       />
-  <Button icon="click" mode="contained" style={{height:"50px"}} onPress={()=>handleSearch(countryName)}>Search</Button>
+              <Button icon="search" mode="contained" style={{height:"50px"}} onPress={()=>setDisplay(true)}>Search</Button>
       </View>
-  <View>{}</View>
+                  <View>
+
+                  </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
